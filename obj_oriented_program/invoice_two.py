454224b5bc680bdc2fce68816a2c83842e16e8a2
jgartsu12 <jgartsu12@gmail.com>
2019-11-19 10:28:29 -0500

    decorators getter and setters

--- obj_oriented_program/invoice_two.py
+++ obj_oriented_program/invoice_two.py
@@ -1,17 +1,32 @@
 class Invoice: 
 
     def __init__(self, client, total): 
-        self.client = client   
-        self.total = total      
+        self._client = client  # protect with _ so child class has access to this data (inheritance) --> data attributte should be protected 
+        self._total = total     
        
     def formatter(self):      
-        return f'{self.client} owes: ${self.total}' 
+        return f'{self._client} owes: ${self._total}' 
+
+    @property
+    def client(self):
+        return self._client
+
+    @client.setter
+    def client(self, client):
+        self._client = client 
+
+    
+    @property
+    def total(self):
+        return self._total
 
 
 google = Invoice('Google', 100) 
 
-print(google.client) 
+print(google.client) # => Google # gives direct acces to client
 
-google.client = 'Yahoo'
+google.client = 'Yahoo' # overrides to yahoo
 
-print(google.client)
+print(google.client)  # => Yahoo
+
+print(google.total) # => 100
