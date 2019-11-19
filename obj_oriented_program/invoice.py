277336164018480e237f54e4a44c2d05ef6f7d1f
jgartsu12 <jgartsu12@gmail.com>
2019-11-19 10:08:17 -0500

    setter

--- obj_oriented_program/invoice.py
+++ obj_oriented_program/invoice.py
@@ -3,18 +3,19 @@ class Invoice: # created class
     def __init__(self, client, total): #constructor fn with instance to class via self arg
         self.client = client   # assign args to the obj # Add data directly to the class
         self.total = total      
-        # getter method (auto done in py)
-    def get_client():
-        return self.client
-
+       
     def formatter(self):      
         return f'{self.client} owes: ${self.total}' # access so self values above
 
 
 google = Invoice('Google', 100) # gets stored in google variable as an object if want access to we have access automcially
 
-print(google.get_client) # => getter method
-print(google.total) # => 100
+print(google.client) # => setter method
+# setter process - go into obj and set the value
+google.client = 'Yahoo'
+
+print(google.client) # => Yahoo overide client value  via setter (setter in python are objs so can reach into and overide the vlaue)
+
 
 #     def greeting():
 #         return 'Hi there'
