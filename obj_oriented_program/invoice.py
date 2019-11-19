e728590c1bf66825679252f3c0782d42d02c95e7
jgartsu12 <jgartsu12@gmail.com>
2019-11-19 10:05:20 -0500

    getter method

--- obj_oriented_program/invoice.py
+++ obj_oriented_program/invoice.py
@@ -3,17 +3,18 @@ class Invoice: # created class
     def __init__(self, client, total): #constructor fn with instance to class via self arg
         self.client = client   # assign args to the obj # Add data directly to the class
         self.total = total      
+        # getter method (auto done in py)
+    def get_client():
+        return self.client
 
     def formatter(self):      
         return f'{self.client} owes: ${self.total}' # access so self values above
 
 
-google = Invoice('Google', 100)
-snapchat = Invoice('Snapchat', 200)
+google = Invoice('Google', 100) # gets stored in google variable as an object if want access to we have access automcially
 
-print(google.formatter()) # => Google owes: $100
-print(snapchat.formatter()) # => Snapchat owes: $200
-
+print(google.get_client) # => getter method
+print(google.total) # => 100
 
 #     def greeting():
 #         return 'Hi there'
