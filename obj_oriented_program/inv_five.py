91d25f83f2b881d3f9418c30d92ee19132de277a
jgartsu12 <jgartsu12@gmail.com>
2019-11-19 11:29:27 -0500

    __iter__

---
+++ obj_oriented_program/inv_five.py
@@ -0,0 +1,85 @@
+# How to Build a Custom Iterator Class in Python
+class Lineup:
+    def __init__(self, players):
+        self.players = players
+
+    def __iter__(self):
+        self.n = 0 # counter variable to keep track of where we are in the line up (index) # n = 0 first time
+        return self
+
+    def __next__(self):
+        if self.n < (len(self.players) - 1): # is n less than length of the list... need -1 not length of list but want the value of index of the last element (tucker which is 8) -> comparing index with last index so use lens -1 => index number of last element
+            player =  self.players[self.n] # grab the player
+            self.n += 1 # makes it iterable to move down the line
+            return player
+        elif self.n == (len(self.players) - 1):
+            player = self.players[self.n]
+            self.n = 0 # starts counter over 
+            return player
+        
+astros = [  # list of strings 
+  'Springer',
+  'Bregman',
+  'Altuve',
+  'Correa',
+  'Reddick',
+  'Gonzalez',
+  'McCann',
+  'Davis',
+  'Tucker'
+]
+
+astros_lineup = Lineup(astros)
+process = iter(astros_lineup)
+
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+print(next(process)) 
+print(next(process))
+ 
+ """ returned: 
+Springer
+Bregman
+Altuve
+Correa
+Reddick
+Gonzalez
+McCann
+Davis
+Tucker
+Springer
+Bregman
+Altuve
+Correa
+Reddick
+Gonzalez
+McCann
+Davis
+Tucker
+Springer
+Bregman
+Altuve
+Correa
+Reddick
+Gonzalez
+"""
