79aa1f08af2c81edc0e76272d8dfe19f315e4c49
jgartsu12 <jgartsu12@gmail.com>
2019-11-19 10:11:21 -0500

    How to Work with Python Properties and Decorators

---
+++ obj_oriented_program/invoice_two.py
@@ -0,0 +1,17 @@
+class Invoice: 
+
+    def __init__(self, client, total): 
+        self.client = client   
+        self.total = total      
+       
+    def formatter(self):      
+        return f'{self.client} owes: ${self.total}' 
+
+
+google = Invoice('Google', 100) 
+
+print(google.client) 
+
+google.client = 'Yahoo'
+
+print(google.client)
