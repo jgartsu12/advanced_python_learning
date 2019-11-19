cc93f8c25da2513d2029a443dd99804ad6e8e69f
jgartsu12 <jgartsu12@gmail.com>
2019-11-19 09:57:50 -0500

    Guide to Python's __init__ Constructor Function

--- obj_oriented_program/invoice.py
+++ obj_oriented_program/invoice.py
@@ -1,4 +1,19 @@
-# class Invoice:
+class Invoice: # created class
+
+    def __init__(self, client, total): #constructor fn with instance to class via self arg
+        self.client = client   # assign args to the obj # Add data directly to the class
+        self.total = total      
+
+    def formatter(self):      
+        return f'{self.client} owes: ${self.total}' # access so self values above
+
+
+google = Invoice('Google', 100)
+snapchat = Invoice('Snapchat', 200)
+
+print(google.formatter()) # => Google owes: $100
+print(snapchat.formatter()) # => Snapchat owes: $200
+
 
 #     def greeting():
 #         return 'Hi there'
@@ -10,17 +25,17 @@
 # inv_two = Invoice()     # instantiate class Invoice again
 # print(inv_two)
 
-class Invoice:
+# class Invoice:
 
-    def greeting(self):
-        return 'Hi there'
+#     def greeting(self):
+#         return 'Hi there'
 
 
-inv_one= Invoice()     # instantiate class Invoice again w/ greeting() fn
-print(inv_one.greeting())
+# inv_one= Invoice()     # instantiate class Invoice again w/ greeting() fn
+# print(inv_one.greeting())
 
-inv_two = Invoice()     # instantiate class Invoice again w/ greeting() fn
-print(inv_two.greeting())
+# inv_two = Invoice()     # instantiate class Invoice again w/ greeting() fn
+# print(inv_two.greeting())
 
 # = >
 # Hi there
